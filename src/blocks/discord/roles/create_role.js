76e85ca670382079d7304e340352e8251f42b1dc
AlexCdDg2 <105122938+AlexCdDg2@users.noreply.github.com>
2022-08-23 22:13:19 -0300

    #mutators_everywhere

--- src/blocks/discord/roles/create_role.js
+++ src/blocks/discord/roles/create_role.js
@@ -1,45 +1,117 @@
 import * as Blockly from "blockly/core";
+import BaseBlockly from "blockly";
+
+
 
 const blockName = "s4d_create_role";
+  const BORDER_FIELDS = ["NAME", "COLOR"];
+const BORDER_TYPES = ["String","Colour"];
+const names = ["with name", "with color"];
+const amountOfInputs = names.length
+const menuName = 'idk'
+
 
 const blockData = {
-    "message0": "%{BKY_CREATE_ROLE}",
+    "message0": "create role in server %1",
     "args0": [
-        {
-            "type": "input_value",
-            "name": "NAME",
-            "check": [ "String", "Number" ]
-        },
         {
             "type": "input_value",
             "name": "SERVER",
             "check": [ "Server" ]
         },
-        {
-            "type":"input_value",
-            "name":"COLOR",
-            "check":"Colour"
-        }
-
-    ],
-    "colour": "#4C97FF",
+      ],
+    "colour": "4C97FF",
     "previousStatement": null,
     "nextStatement": null,
-    "inputsInline": false,
-    "tooltip": "",
     "helpUrl": ""
 };
 
-Blockly.Blocks[blockName] = {
-    init: function() {
-        this.jsonInit(blockData);
+
+Blockly.Blocks[menuName] = {
+    init: function () {
+        this.setColour("4C97FF");
+        this.setHelpUrl("");
     }
 };
+Blockly.Blocks[blockName] = {
+    init: function () {
+        this.jsonInit(blockData);
+        this.setMutator(new Blockly.Mutator([]));
+        this.inputs_ = []
+        for (let i = 0; i < amountOfInputs; i++) {
+            this.inputs_.push(false)
+        }
+    },
 
-Blockly.JavaScript[blockName] = function(block){
-    const name = Blockly.JavaScript.valueToCode(block, "NAME", Blockly.JavaScript.ORDER_ATOMIC);
-    const server = Blockly.JavaScript.valueToCode(block, "SERVER", Blockly.JavaScript.ORDER_ATOMIC);
-    const color = Blockly.JavaScript.valueToCode(block, "COLOR", Blockly.JavaScript.ORDER_ATOMIC)
-    const code = `${server}.roles.create({ name: ${name},color:${color} });\n`;
-    return code;
+
+    mutationToDom: function () {
+        if (!this.inputs_) {
+            return null;
+        }
+        const container = document.createElement("mutation");
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if (this.inputs_[i]) container.setAttribute(BORDER_FIELDS[i], this.inputs_[i])
+        }
+        return container;
+    },
+
+    domToMutation: function (xmlElement) {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = xmlElement.getAttribute(BORDER_FIELDS[i].toLowerCase()) == "true";
+        }
+        this.updateShape_();
+    },
+
+    decompose: function (workspace) {
+        const containerBlock = workspace.newBlock(menuName);
+        for (let i = 0; i < this.inputs_.length; i++) {
+            BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+            containerBlock.appendDummyInput()
+                .setAlign(Blockly.ALIGN_LEFT)
+                .appendField(new Blockly.FieldCheckbox(this.inputs_[i] ? "TRUE" : "FALSE"), BORDER_FIELDS[i].toUpperCase())
+                .appendField(names[i])
+        }
+        containerBlock.initSvg();
+        return containerBlock;
+    },
+
+    compose: function (containerBlock) {
+        // Set states
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = (containerBlock.getFieldValue(BORDER_FIELDS[i].toUpperCase()) == "TRUE");
+        }
+        this.updateShape_();
+    },
+
+    updateShape_: function () {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((!this.inputs_[i]) && (this.getInput(BORDER_FIELDS[i].toUpperCase()))) this.removeInput(BORDER_FIELDS[i].toUpperCase());
+        }
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((this.inputs_[i]) && (!(this.getInput(BORDER_FIELDS[i].toUpperCase())))) {
+                BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+                this.appendValueInput(BORDER_FIELDS[i].toUpperCase())
+                    .setCheck(BORDER_TYPES[i])
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(names[i]);
+            }
+        }
+    }
+
+};
+
+Blockly.JavaScript[blockName] = function (block) {
+const server = Blockly.JavaScript.valueToCode(block, "SERVER", Blockly.JavaScript.ORDER_NONE);
+ const name = Blockly.JavaScript.valueToCode(block, "NAME", Blockly.JavaScript.ORDER_NONE);
+    const color = Blockly.JavaScript.valueToCode(block, "COLOR", Blockly.JavaScript.ORDER_NONE);
+var code = `${server}.roles.create({`
+    if(name) {
+      code += `name: ${name},`
+    }
+  if(color) {
+    code += `color: ${color}`
+  }
+
+  var finalcode = `${code}}); \n`;
+    return finalcode;
 };
