913933b3bd1d6302ea0849cec2bdf19de323aa15
AlexCdDg2 <105122938+AlexCdDg2@users.noreply.github.com>
2022-08-23 23:40:42 -0300

    #mutators_everywhere

--- src/blocks/Eruption/message_embed/embed_set_footer.js
+++ src/blocks/Eruption/message_embed/embed_set_footer.js
@@ -1,44 +1,115 @@
 import * as Blockly from "blockly/core";
+import BaseBlockly from "blockly";
 import { registerRestrictions } from "../../../restrictions";
 
 const blockName = "s4d_embed_set_footer";
+  const BORDER_FIELDS = ["ICON"];
+const BORDER_TYPES = [""];
+const names = ["set icon"];
+const amountOfInputs = names.length
+const menuName = 'idk'
+
 
 const blockData = {
-    "message0": "set embed footer %1 icon (optional) %2",
-    "args0": [
+    "message0": "set embed footer %1 ",
+  "args0": [
       {
         "type": "input_value",
         "name": "FOOTER"
       },
-      {
-        "type": "input_value",
-        "name": "ICON"
-      }
     ],
+    "colour": "40BF4A",
     "previousStatement": null,
     "nextStatement": null,
-    "colour": "#40BF4A",
-    "tooltip": "Set the footer of an embed.",
     "helpUrl": ""
 };
 
+
+Blockly.Blocks[menuName] = {
+    init: function () {
+        this.setColour("#40BF4A");
+        this.setHelpUrl("");
+    }
+};
 Blockly.Blocks[blockName] = {
-    init: function() {
+    init: function () {
         this.jsonInit(blockData);
+        this.setMutator(new Blockly.Mutator([]));
+        this.inputs_ = []
+        for (let i = 0; i < amountOfInputs; i++) {
+            this.inputs_.push(false)
+        }
+    },
+
+
+    mutationToDom: function () {
+        if (!this.inputs_) {
+            return null;
+        }
+        const container = document.createElement("mutation");
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if (this.inputs_[i]) container.setAttribute(BORDER_FIELDS[i], this.inputs_[i])
+        }
+        return container;
+    },
+
+    domToMutation: function (xmlElement) {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = xmlElement.getAttribute(BORDER_FIELDS[i].toLowerCase()) == "true";
+        }
+        this.updateShape_();
+    },
+
+    decompose: function (workspace) {
+        const containerBlock = workspace.newBlock(menuName);
+        for (let i = 0; i < this.inputs_.length; i++) {
+            BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+            containerBlock.appendDummyInput()
+                .setAlign(Blockly.ALIGN_LEFT)
+                .appendField(new Blockly.FieldCheckbox(this.inputs_[i] ? "TRUE" : "FALSE"), BORDER_FIELDS[i].toUpperCase())
+                .appendField(names[i])
+        }
+        containerBlock.initSvg();
+        return containerBlock;
+    },
+
+    compose: function (containerBlock) {
+        // Set states
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = (containerBlock.getFieldValue(BORDER_FIELDS[i].toUpperCase()) == "TRUE");
+        }
+        this.updateShape_();
+    },
+
+    updateShape_: function () {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((!this.inputs_[i]) && (this.getInput(BORDER_FIELDS[i].toUpperCase()))) this.removeInput(BORDER_FIELDS[i].toUpperCase());
+        }
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((this.inputs_[i]) && (!(this.getInput(BORDER_FIELDS[i].toUpperCase())))) {
+                BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+                this.appendValueInput(BORDER_FIELDS[i].toUpperCase())
+                    .setCheck(BORDER_TYPES[i])
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(names[i]);
+            }
+        }
     }
+
 };
 
-Blockly.JavaScript[blockName] = function(block){
-     const footer = Blockly.JavaScript.valueToCode(block, "FOOTER", Blockly.JavaScript.ORDER_ATOMIC);
-    const icon = Blockly.JavaScript.valueToCode(block, "ICON", Blockly.JavaScript.ORDER_ATOMIC);
-    if (icon.length == 0) {
-      const code = `embed.setFooter({text: String(${footer})}) \n`; 
-    return code;
+Blockly.JavaScript[blockName] = function (block) {
+  const icon = Blockly.JavaScript.valueToCode(block, "ICON", Blockly.JavaScript.ORDER_NONE)
+    const footer = Blockly.JavaScript.valueToCode(block, "FOOTER", Blockly.JavaScript.ORDER_ATOMIC);
+
+
+var code = `embed.setFooter({text: String(${footer})}) \n`
+    if(icon) {
+      code = `embed.setFooter({text: String(${footer}), iconURL: String(${icon})}); \n`
     }
-  const code = `embed.setFooter({text: String(${footer}), iconURL: String(${icon})}); \n`; 
+
     return code;
 };
-
 
 registerRestrictions(blockName, [
     {
