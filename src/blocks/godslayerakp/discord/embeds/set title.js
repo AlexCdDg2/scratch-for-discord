386ca292091e1f3dc73dd35c3e9541bbe7685040
AlexCdDg2 <105122938+AlexCdDg2@users.noreply.github.com>
2022-08-23 23:50:41 -0300

    #mutators_everywhere

--- src/blocks/godslayerakp/discord/embeds/set title.js
+++ src/blocks/godslayerakp/discord/embeds/set title.js
@@ -1,48 +1,115 @@
-import Blockly from "blockly/core";
-import { registerRestrictions } from "../../../../restrictions";
+import * as Blockly from "blockly/core";
+import BaseBlockly from "blockly";
+
 const blockName = "gsa_set_embed_title";
+  const BORDER_FIELDS = ["URL"];
+const BORDER_TYPES = [""];
+const names = ["set url"];
+const amountOfInputs = names.length
+const menuName = 'idk'
+
+
 const blockData = {
-  "type": "gsa_set_embed_title",
-  "message0": "set embed title %1 url %2",
+    "message0": "set embed title %1",
   "args0": [
     {
       "type": "input_value",
-      "name": "title",
+      "name": "TITLE",
       "check": "String"
     },
-    {
-      "type": "input_value",
-      "name": "url",
-      "check": "String"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "inputsInline": true,
-  "colour": 120,
-  "tooltip": "must be in a make embed with name block",
-  "helpUrl": ""
+    ],
+    "colour": 120,
+    "previousStatement": null,
+    "nextStatement": null,
+    "tooltip": "must be in a make embed with name block",
+    "helpUrl": ""
 };
 
+
+Blockly.Blocks[menuName] = {
+    init: function () {
+        this.setColour(120);
+        this.setHelpUrl("");
+    }
+};
 Blockly.Blocks[blockName] = {
-  init: function () {
-    this.jsonInit(blockData);
-  }
+    init: function () {
+        this.jsonInit(blockData);
+        this.setMutator(new Blockly.Mutator([]));
+        this.inputs_ = []
+        for (let i = 0; i < amountOfInputs; i++) {
+            this.inputs_.push(false)
+        }
+    },
+
+
+    mutationToDom: function () {
+        if (!this.inputs_) {
+            return null;
+        }
+        const container = document.createElement("mutation");
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if (this.inputs_[i]) container.setAttribute(BORDER_FIELDS[i], this.inputs_[i])
+        }
+        return container;
+    },
+
+    domToMutation: function (xmlElement) {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = xmlElement.getAttribute(BORDER_FIELDS[i].toLowerCase()) == "true";
+        }
+        this.updateShape_();
+    },
+
+    decompose: function (workspace) {
+        const containerBlock = workspace.newBlock(menuName);
+        for (let i = 0; i < this.inputs_.length; i++) {
+            BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+            containerBlock.appendDummyInput()
+                .setAlign(Blockly.ALIGN_LEFT)
+                .appendField(new Blockly.FieldCheckbox(this.inputs_[i] ? "TRUE" : "FALSE"), BORDER_FIELDS[i].toUpperCase())
+                .appendField(names[i])
+        }
+        containerBlock.initSvg();
+        return containerBlock;
+    },
+
+    compose: function (containerBlock) {
+        // Set states
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = (containerBlock.getFieldValue(BORDER_FIELDS[i].toUpperCase()) == "TRUE");
+        }
+        this.updateShape_();
+    },
+
+    updateShape_: function () {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((!this.inputs_[i]) && (this.getInput(BORDER_FIELDS[i].toUpperCase()))) this.removeInput(BORDER_FIELDS[i].toUpperCase());
+        }
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((this.inputs_[i]) && (!(this.getInput(BORDER_FIELDS[i].toUpperCase())))) {
+                BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+                this.appendValueInput(BORDER_FIELDS[i].toUpperCase())
+                    .setCheck(BORDER_TYPES[i])
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(names[i]);
+            }
+        }
+    }
+
 };
 
 Blockly.JavaScript[blockName] = function (block) {
-  const color = Blockly.JavaScript.valueToCode(block, "title", Blockly.JavaScript.ORDER_ATOMIC)
-  const url = Blockly.JavaScript.valueToCode(block, "url", Blockly.JavaScript.ORDER_ATOMIC)
-  return `title: String(${color}),
+  const url = Blockly.JavaScript.valueToCode(block, "URL", Blockly.JavaScript.ORDER_NONE)
+    const title = Blockly.JavaScript.valueToCode(block, "TITLE", Blockly.JavaScript.ORDER_ATOMIC);
+
+
+var code = `title: String(${title}),`
+    if(url) {
+      code = `title: String(${title}),
 url: String(${url}),
 `
-}
-registerRestrictions(blockName, [
-  {
-    type: "hasparent",
-    message: "$This block needs to be placed in a \"create embed with name () then\" block!",
-    types: [
-      "gsa_create_embed"
-    ]
-  }
-]);
+    }
+
+    return code;
+};
