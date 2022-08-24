9c1a97ae2fe78e567fc1c38cd36db5997d175c69
AlexCdDg2 <105122938+AlexCdDg2@users.noreply.github.com>
2022-08-23 23:10:53 -0300

    #mutators_everywhere

--- src/blocks/ahq/buttons/update.js
+++ src/blocks/ahq/buttons/update.js
@@ -1,70 +1,118 @@
-import * as Blockly from "blockly";
+import * as Blockly from "blockly/core";
+import BaseBlockly from "blockly";
+
 const blockName = "updte_ahq_button";
-const ahqcolor = ['#33cc00', '#33cc00', '#33cc00', '#33cc00'];
-function listsGetRandomItem(list, remove) {
-    var x = Math.floor(Math.random() * list.length);
-    if (remove) {
-        return list.splice(x, 1)[0];
-    } else {
-        return list[x];
-    }
-}
+  const BORDER_FIELDS = ["CONTENT", "BUTTON", "EMBED"];
+const BORDER_TYPES = ["String","String", ""];
+const names = ["content", "button", "embed"];
+const amountOfInputs = names.length
+const menuName = 'idk'
+
+
 const blockData = {
-    "message0": "update %1 %2 %3 button %4 %5 embeds %6",
-    "args0": [
-        {
-            "type": "input_dummy"
-        },
-        {
-            "type": "input_value",
-            "name": "Label",
-            "check": ["String", "Integer"]
-        },
-        {
-            "type": "input_dummy"
-        },
-        {
-            "type": "input_value",
-            "name": "button val",
-            "check": "String"
-        },
-        {
-            "type": "input_dummy"
-        },
-        {
-            "type": "input_value",
-            "name": "embed val",
-            "check": ""
-        },
-    ],
-    "colour": listsGetRandomItem(ahqcolor, false),
+    "message0": "update",
+    "colour": "33cc00",
     "previousStatement": null,
     "nextStatement": null,
+    "inputsInline": true,
+    "helpUrl": ""
 };
 
+
+Blockly.Blocks[menuName] = {
+    init: function () {
+        this.setColour("33cc00");
+        this.setHelpUrl("");
+    }
+};
 Blockly.Blocks[blockName] = {
     init: function () {
         this.jsonInit(blockData);
+        this.setMutator(new Blockly.Mutator([]));
+        this.inputs_ = []
+        for (let i = 0; i < amountOfInputs; i++) {
+            this.inputs_.push(false)
+        }
+    },
+
+
+    mutationToDom: function () {
+        if (!this.inputs_) {
+            return null;
+        }
+        const container = document.createElement("mutation");
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if (this.inputs_[i]) container.setAttribute(BORDER_FIELDS[i], this.inputs_[i])
+        }
+        return container;
+    },
+
+    domToMutation: function (xmlElement) {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = xmlElement.getAttribute(BORDER_FIELDS[i].toLowerCase()) == "true";
+        }
+        this.updateShape_();
+    },
+
+    decompose: function (workspace) {
+        const containerBlock = workspace.newBlock(menuName);
+        for (let i = 0; i < this.inputs_.length; i++) {
+            BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+            containerBlock.appendDummyInput()
+                .setAlign(Blockly.ALIGN_LEFT)
+                .appendField(new Blockly.FieldCheckbox(this.inputs_[i] ? "TRUE" : "FALSE"), BORDER_FIELDS[i].toUpperCase())
+                .appendField(names[i])
+        }
+        containerBlock.initSvg();
+        return containerBlock;
+    },
+
+    compose: function (containerBlock) {
+        // Set states
+        for (let i = 0; i < this.inputs_.length; i++) {
+            this.inputs_[i] = (containerBlock.getFieldValue(BORDER_FIELDS[i].toUpperCase()) == "TRUE");
+        }
+        this.updateShape_();
+    },
+
+    updateShape_: function () {
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((!this.inputs_[i]) && (this.getInput(BORDER_FIELDS[i].toUpperCase()))) this.removeInput(BORDER_FIELDS[i].toUpperCase());
+        }
+        for (let i = 0; i < this.inputs_.length; i++) {
+            if ((this.inputs_[i]) && (!(this.getInput(BORDER_FIELDS[i].toUpperCase())))) {
+                BaseBlockly.Msg[BORDER_FIELDS[i]] = names[i];
+                this.appendValueInput(BORDER_FIELDS[i].toUpperCase())
+                    .setCheck(BORDER_TYPES[i])
+                    .setAlign(Blockly.ALIGN_RIGHT)
+                    .appendField(names[i]);
+            }
+        }
     }
+
 };
+
 Blockly.JavaScript[blockName] = function (block) {
-    var ahq = ``;
-    let extra = "";
-    const data = Blockly.JavaScript.valueToCode(block, "Label", Blockly.JavaScript.ORDER_NONE);
-    const statementsThen = Blockly.JavaScript.valueToCode(block, "button val", Blockly.JavaScript.ORDER_NONE);
-    const embed = Blockly.JavaScript.valueToCode(block, "embed val", Blockly.JavaScript.ORDER_NONE);
-    if (statementsThen) {
-        ahq = `components: [new MessageActionRow().addComponents(
-            ${statementsThen.replace("'", "").replace("'", "")}
+  const content = Blockly.JavaScript.valueToCode(block, "CONTENT", Blockly.JavaScript.ORDER_NONE)
+const button = Blockly.JavaScript.valueToCode(block, "BUTTON", Blockly.JavaScript.ORDER_NONE)
+    const embed = Blockly.JavaScript.valueToCode(block, "EMBED", Blockly.JavaScript.ORDER_NONE);
+
+var ahq;
+  
+var code = `i.update({`
+    if(content) {
+      code += `content: String(${content}),`
+    }
+  if(button) {
+    ahq = `components: [new MessageActionRow().addComponents(
+            ${button.replace("'", "").replace("'", "")}
         )],`;
-    }
-    if (embed) {
-        extra = `${embed}`;
-    }
-    const code = `i.update({
-        content: String(${data}),
-        ${ahq.replace("`", "").replace("`", "")}
-        ${extra.replace("`", "").replace("`", "").replace("'", "").replace("'", "")}
-        });`;
-    return code;
+    code += `${ahq.replace("`", "").replace("`", "")}`
+  }
+if(embed) {
+    code += `embeds: [${embed.replace("`", "").replace("`", "").replace("'", "").replace("'", "")}]`
+  } 
+  var finalcode = `${code}}); \n`;
+    return finalcode;
 };
+
